95602ac4d292e0af31adb0fe8b172dbc914f8eec
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-06-26 13:03:34 -0700

    Safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!usr/bin/python3
 def safe_print_integer(value):
-    
+
     try:
         print("{:d}".format(value))
-        return True
-    
+        return (True)
+
     except (ValueError, TypeError):
-        return False
+        return (False)
