04c468fd0a46a2d04f947c1b721073599afefec2
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-09-13 15:08:30 -0700

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
-    if (searchElement === list[i]) {
+    if (list[i] === searchElement) {
       nOccurrences++;
     }
   }
